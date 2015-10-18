8bec8ab2c561c7ab5791dc6515934b1ffe027ec5
mtboensel <rockviewoj@hotmail.com>
2015-10-18 16:17:13 -0700

    Some User Interaction
    Added some code for the user to interact with

--- test.py
+++ test.py
@@ -21,11 +21,47 @@ def scaleVal(soundArray, scale):
         sample[...] = sample * scale
 
     return soundArray
+
+
+#allows user to choose what theyre doing
+while True:
+    select = input('Choose 0 for ____ 1 for ___ 2 for ______: ')
+
+    #why can't Python do switch cases aaaah
+    if(select == '0'):
+        someBool = True
+        break
+
+    elif(select == '1'):
+        someOtherBool = True
+        break
+
+    elif(select == '2'):
+        lastBool = True
+
+    else:
+        print("Please choose one of the specified options (0/1/2)")
+
+while True:
+    fileName = input('Specify .wav file: ')
+
+    if(fileName[-4:] != ".wav"):
+        print("Please specify .wav files only")
+
+    else:
+        break
     
-sampleRate, data = wavfile.read('creak.wav')
+    
+#read in sample file    
+sampleRate, data = wavfile.read(fileName)
 
 
-data = trigApply(data, 1)
+#do manip
 
+
+#call function
+data = scaleVal(data, -1.5)
+
+#write to test.wav
 wavfile.write('test.wav', sampleRate, data)
 
